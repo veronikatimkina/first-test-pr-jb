d0c47b676055e08a8d37e77675f9f179614743e5
Timkina Veronika <vtimkina@marathonbet.ru>
2020-04-18 19:12:51 +0300

    add new class from testacc

--- src/main/java/TestaccClass.java
+++ src/main/java/TestaccClass.java
@@ -1,12 +1,6 @@
-package main.java;
-
 public class TestaccClass {
     public static void main(String[] args) {
-        int number = 12345678;
-        System.out.println("number = " + number);
+        int number = 123;
+        System.out.println(number);
     }
-
-
-
-
 }
