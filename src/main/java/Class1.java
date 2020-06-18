23e5a49dc0120432784254c91c71b354f7e3f14c
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-18 14:04:12 +0300

    abc

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,5 +1,7 @@
-﻿public class Class1 {
-    public void foo(){}
+public class Class1 {
+    public void foo(){
+        int i =1;
+    }
 
     public static void main(String[] args) {
         System.out.println("");
