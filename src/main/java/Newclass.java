3c5c0da5f9b79580f3768daa97232ee3bd7a65e6
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-23 18:14:46 +0300

    commit from testEAP2 2

--- src/main/java/Newclass.java
+++ src/main/java/Newclass.java
@@ -1,7 +1,7 @@
-package main.java;
-
 public class Newclass {
     public static void main(String[] args){
-        System.out.println("Nothing");
+
+        System.out.println("ethrth");
+
     }
 }
