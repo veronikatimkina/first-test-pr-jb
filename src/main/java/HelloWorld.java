42a6810d25c2ca8e6a5d7ba6f8cc0391c2d9a4f9
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-07-29 14:37:31 +0300

    edit HelloWorld in master

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,5 @@
-package main.java;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("sssswffffefwwe");
     }
 }
