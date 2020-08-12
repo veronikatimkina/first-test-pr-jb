76ba2904fa92154fa632fa67a0b549ca2170b659
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-08-12 19:28:38 +0300

    test file links

--- src/main/java/pack/Class5.java
+++ src/main/java/pack/Class5.java
@@ -1,6 +1,10 @@
 package pack;
 
 public class Class5 {
+    public Class5() {
+        super();
+    }
+
     public static void main(String[] args) {
 
     }
--- src/main/java/pack/Class6.java
+++ src/main/java/pack/Class6.java
@@ -1,7 +1,5 @@
 package pack;
 
 public class Class6 {
-    public static void main(String[] args) {
 
-    }
 }
