01f47a254bb8b9df57d0b6f109090703ed6c8e45
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-09-30 16:50:39 +0300

    test PRs branches

--- src/TestBr.java
+++ src/TestBr.java
@@ -1,18 +1,2 @@
 public class TestBr {
-    
-    public static void main(String[] args) {
-        System.out.println(test(22));
-    }
-
-    public static int test(int a){
-        return a;
-    }
-
-    public int test2(int a){
-        return a;
-    }
-
-    public int test3(int a){
-        return a;
-    }
 }
