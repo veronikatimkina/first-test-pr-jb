1217fb9ea39e8738d83360b9c9d6b44b99f39232
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-12-29 17:13:19 +0300

    test commit

---
+++ src/main/java/ClassTest.java
@@ -0,0 +1,5 @@
+package main.java;
+
+class ClassTest {
+public void foo(){}
+}
--- src/main/java/pack/Class16.java
+++ src/main/java/pack/Class16.java
@@ -1,7 +1,7 @@
-package pack;
+package main.java.pack;
 
 public class Class16 {
     public static void main(String[] args) {
-
+        System.out.println("frfrfr");
     }
 }
