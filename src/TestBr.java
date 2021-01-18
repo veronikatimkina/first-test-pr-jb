f426b443ca62d6fb6d34b1d235e904f4e441d581
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2021-01-18 14:19:33 +0300

    123

--- src/TestBr.java
+++ src/TestBr.java
@@ -1,6 +1,10 @@
 public class TestBr {
     
     public static void main(String[] args) {
+        System.out.println("gfuyucyedcgwdcwce");
+    }
 
+    public boolean test(boolean a){
+        return a;
     }
 }
