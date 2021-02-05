fd97fa848e8d6c65c7b8a94b2690e65c765cb3b8
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2021-02-05 19:21:59 +0300

    commit to test github

--- src/TestBr.java
+++ src/TestBr.java
@@ -1,22 +1,14 @@
 public class TestBr {
     
     public static void main(String[] args) {
-        System.out.println(test(1));
+        System.out.println("ввв");
     }
 
-    public static int test(int a){
+    public boolean test(boolean a){
         return a;
     }
 
-    public int test2(int a){
-        return a;
-    }
-
-    public int test3(int a){
-        return a;
-    }
-
-    public int test4(int a){
-        return a;
-    }
+public int dede(int b){
+        return b;
+}
 }
