60c7bd2195682a5974d56a160efa504980b07d4c
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-03-09 19:41:33 +0300

    delete files

--- src/TestBr.java
+++ src/TestBr.java
@@ -1,7 +1,7 @@
 public class TestBr {
     
     public static void main(String[] args) {
-        System.out.println(test(1));
+        System.out.println(test(22));
     }
 
     public static int test(int a){
@@ -13,10 +13,6 @@ public class TestBr {
     }
 
     public int test3(int a){
-        return a;
-    }
-
-    public int test4(int a){
         return a;
     }
 }
