10f8ab2ea97f7b290e6794fcbe2e2aac6330f024
testaccount120 <63915539+testaccount120@users.noreply.github.com>
2021-03-12 20:35:01 +0300

    Merge pull request #88
    * message commit
    
    * message commit
    
    * message commit
    
    * message commit
    
    * message commit
    
    * message commit
    
    * message commit
    
    * message commit
    
    * message commit
    
    * changes3g34g4t
    
    * changes3g34g4t
    
    * some long commit message to check pull request creation blablabla
    
    * some long commit message to check pull request creation blablabla

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
+package main.java;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("sssswffffefwwe");
+        System.out.println("wgrwrgrgerg");
     }
 }
