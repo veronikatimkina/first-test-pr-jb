7f267b5e6c30d4238220be76b7c3213d86095d7f
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-04-09 19:54:58 +0300

    commit in m

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("wgrwrgrgerg");
+        System.out.println("000000000000000");
     }
 }
