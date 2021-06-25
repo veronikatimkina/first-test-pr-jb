veronikatimkina/first-test-pr-jb-withpom

commit in two repos
commit in second repo 2

commit in second repo

src/main/java/Class2.java
@@ -1,5 +1,5 @@
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("dfbdgbdb");
+        System.out.println("ggggg");
     }
 }

veronikatimkina/first-test-pr-jb

src/main/java/Class2.java
@@ -1,5 +1,5 @@
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("gggbgergregregegg");
+        System.out.println("dfbdgbdb");
     }
 }