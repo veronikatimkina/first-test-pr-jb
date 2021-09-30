veronikatimkina/first-test-pr-jb-withpom

commit from master with conflict

src/main/java/Class2.java
@@ -1,5 +1,5 @@
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("ggggg");
+        System.out.println("gggbgergregregegg");
     }
 }

veronikatimkina/first-test-pr-jb

src/main/java/Class2.java
@@ -1,5 +1,5 @@
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("dfbdgbdb");
+        System.out.println("ggggg");
     }
 }