52f0b16297ae2544ea00202fabfddf0568f0d00c
Veronika.Timkina <veronika.timkina@jetbrains.com>
2022-01-10 13:13:55 +0300

    1

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,6 +1,6 @@
 public class Class1 {
 
     public static void main(String[] args) {
-        System.out.println("rgeаа333ааgergerge");
+        System.out.println("rgeаа33343t34t34t34tааgergerge");
     }
 }
