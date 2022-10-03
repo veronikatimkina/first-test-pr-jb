ff2ba9cb4c8c43c1244f1c315168d7d4569f2e4e
Veronika.Timkina <veronika.timkina@jetbrains.com>
2022-10-03 11:34:17 +0200

    commit to create PR

--- src/main/java/Class002.java
+++ src/main/java/Class002.java
@@ -2,7 +2,7 @@ package main.java;
 //ewwefwefew
 public class Class002 {
     public static void main(String[] args) {
-        System.out.println("Iggg <branch> is specifiederherherh, git rebase will perform an automatic git switch <branch> before doing anything else. Otherwise it remains on the current branch.\n" +
+        System.out.println("Iggg <branch> is specified, git rebase will perform an automatic git switch <branch> before doing anything else. Otherwise it remains on the current branch.\n" +
                 "\n" +
                 "If <upstream> is not specified, the upstream configured in branch.<name>.remote and branch.<name>.merge options will be used (see git-config[1] for details) and the --fork-point option is assumed. If you are currently not on any branch or if the current branch does not have a configured upstream, the rebase will abort.");
 
@@ -10,5 +10,4 @@ public class Class002 {
                 "\n" +
                 "The current branch is reset to <upstream>, or <newbase> if the --onto option was supplied. This has the exact same effect as git reset --hard <upstream> (or <newbase>). ORIG_HEAD is set to point at the tip of the branch before the reset.");
     }
-    //evwevwewefeegwgeefrennn
 }
