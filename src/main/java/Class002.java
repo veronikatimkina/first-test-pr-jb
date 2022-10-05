d129811545fd7d4129991c67c95033684dd667f3
test nika <texst@jetbrains.com>
2022-10-05 18:41:30 +0200

    commit to create PR

--- src/main/java/Class002.java
+++ src/main/java/Class002.java
@@ -1,5 +1,5 @@
 package main.java;
-//ewwefwefew
+//ewwefwefewreverberbe
 public class Class002 {
     public static void main(String[] args) {
         System.out.println("If <branch> is specified  git rebase will perform an automatic git switch <branch> before doing anything else. Otherwise it remains on the current branch.\n" +
