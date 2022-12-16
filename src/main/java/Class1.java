b837319cebf604ec4db37f3cc6f87920fff38688
test nika <texst@jetbrains.com>
2022-12-16 20:00:46 +0100

    commit to create PR IDEA-123123

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -3,6 +3,6 @@ package main.java;
 public class Class1 {
 
     public static void main(String[] args) {
-        System.out.println("rgeаа333r2r23r23r3ааgergerge");
+        System.out.println("rgeаа333r2r23r23r3ааgersdvsdssdvsdvgerge");
     }
 }
