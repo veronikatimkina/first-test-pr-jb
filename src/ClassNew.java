987b49d9f69e8c930b8eff4da013270040ee5880
Mirza Suljić <veronika.timkina@jetbrains.com>
2023-03-06 13:20:43 +0100

    new commit

--- src/ClassNew.java
+++
@@ -1,3 +0,0 @@
-public class ClassNew {
-    //new line
-}
--- src/main/java/Class9_renamed.java
+++ src/main/java/Class9_renamed.java
@@ -1,5 +1,5 @@
 public class Class9_renamed {
 
 //changessbgsdgdfgethrerthrhtrhsdvvgrvgerverv
-    //changes
+    //changesacfewfwefwef
 }
