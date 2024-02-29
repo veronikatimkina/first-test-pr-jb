b2c1ebfea3c58914a55e3f7a09aaa09d91aaf887
Veronika Leko <veronika.timkina@jetbrains.com>
2024-02-29 17:36:06 +0100

    123dbsdbsdbsddbvsdbsdbsd

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -3,6 +3,6 @@ package main.java;
 public class Class1 {
 
     public static void main(String[] args) {
-        System.out.println("rgeаа333r2r23r23r3ааgersdvsdssdvsdvgerge");
+        System.out.println("rgeаа333r2r23eferfrferferferferfr23r3ааgersdvsdssdvsdvgerge");
     }
 }
